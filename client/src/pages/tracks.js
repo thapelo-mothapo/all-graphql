72fecf45e55454fe704d906449685bf8136419e6
Thapelo <dev.mothapotr@gmail.com>
2022-09-16 21:54:48 +0200

    feat(client): query server data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
+import { gql, useQuery } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+const TRACKS = gql`
+  query Tracks {
+    tracks {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error } = useQuery(TRACKS);
+  if (loading) return "Loading...";
+
+  if (error) return `Error ${error}`;
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracks?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
